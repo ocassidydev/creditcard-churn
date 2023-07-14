9b1b93940824d1e4fea43ba463686be29d93b0c9
ocassidydev <ocassidy@tcd.ie>
2023-07-14 17:32:04 +0000

    Add streamlit app test

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from app_pages.multipage import MultiPage 
+
+# load page scripts
+
+app = MultiPage(app_name = "Mushroom Safety")
---
+++ app_pages/page_summary.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def page_summary_body():
+    
+    st.write("### Project Summary")
+
+    st.info(
+        f"test")
+
+    st.write(
+        f"* For additional information, please visit and read the "
+        f"[Project README file](https://github.com/ocassidydev/mushroom-safety)."
+    )
+
+    st.success(
+        "test"
+    )
