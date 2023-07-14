7069e8f585ea04dfee56c665dc350b41cc80436b
ocassidydev <ocassidy@tcd.ie>
2023-07-14 18:13:57 +0000

    Test streamlit dashboard

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from app_pages.multipage import MultiPage 
 
 # load page scripts
-from app_pages.page_summmary import page_summary_body
+from app_pages.page_summary import page_summary_body
 
 app = MultiPage(app_name = "Mushroom Safety")
 
