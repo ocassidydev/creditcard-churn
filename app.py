b559d33464689be8e5f94ad81dc5280c85886960
ocassidydev <ocassidy@tcd.ie>
2023-07-14 17:35:02 +0000

    Add app.py for streamlit dashboard

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ import streamlit as st
 from app_pages.multipage import MultiPage 
 
 # load page scripts
+from app_pages.page_summmary import page_summary_body
 
 app = MultiPage(app_name = "Mushroom Safety")
+
+app.add_page("Project Summary", page_summary_body)
+
+app.run()
