7665f8237feed28e95a40c6274b6479b4704c3f5
ocassidydev <>
2023-07-28 16:49:12 +0000

    Fix lines in page_predict_edible

--- app_pages/page_predict_edible.py
+++ app_pages/page_predict_edible.py
@@ -4,26 +4,27 @@ import matplotlib.pyplot as plt
 from src.data_management import load_mushroom_data, load_pkl_file
 from src.machine_learning.evaluate_clf import clf_performance
 
+
 def page_predict_edible_body():
 
     version = 'v1'
     # load needed files
     churn_pipe_dc_fe = load_pkl_file(
-        f'outputs/ml_pipeline/predict_churn/{version}/clf_pipeline_data_cleaning_feat_eng.pkl')
+        f'outputs/ml_pipeline/predict_edible/{version}/clf_pipeline_data_cleaning_feat_eng.pkl')
     churn_pipe_model = load_pkl_file(
-        f"outputs/ml_pipeline/predict_churn/{version}/clf_pipeline_model.pkl")
+        f"outputs/ml_pipeline/predict_edible/{version}/clf_pipeline_model.pkl")
     churn_feat_importance = plt.imread(
-        f"outputs/ml_pipeline/predict_churn/{version}/features_importance.png")
+        f"outputs/ml_pipeline/predict_edible/{version}/features_importance.png")
     X_train = pd.read_csv(
-        f"outputs/ml_pipeline/predict_churn/{version}/X_train.csv")
+        f"outputs/ml_pipeline/predict_edible/{version}/X_train.csv")
     X_test = pd.read_csv(
-        f"outputs/ml_pipeline/predict_churn/{version}/X_test.csv")
+        f"outputs/ml_pipeline/predict_edible/{version}/X_test.csv")
     y_train = pd.read_csv(
-        f"outputs/ml_pipeline/predict_churn/{version}/y_train.csv").values
+        f"outputs/ml_pipeline/predict_edible/{version}/y_train.csv").values
     y_test = pd.read_csv(
-        f"outputs/ml_pipeline/predict_churn/{version}/y_test.csv").values
+        f"outputs/ml_pipeline/predict_edible/{version}/y_test.csv").values
 
-    st.write("### ML Pipeline: Predict Prospect Churn")
+    st.write("### ML Pipeline: Predict Mushroom Edibility")
     # display pipeline training summary conclusions
     st.info(
         f"* The pipeline was tuned aiming at least 1.00 precision on 'edible' class, "
@@ -45,7 +46,7 @@ def page_predict_edible_body():
 
     # show feature importance plot
     st.write("---")
-    st.write("* The features the model was trained and their importance.")
+    st.write("* The features the model was trained on and their importance.")
     st.write(X_train.columns.to_list())
     st.image(churn_feat_importance)
 
