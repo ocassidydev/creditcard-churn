cb9c976cee21af9fc8255bc68431473278d6d4a7
ocassidydev <ocassidy@tcd.ie>
2023-07-28 15:46:19 +0000

    Add data_management.py to src

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from app_pages.multipage import MultiPage
 # load page scripts
 from app_pages.page_summary import page_summary_body
 
+
+
+from app_pages.page_predict_edible import page_predict_edible_body
+
 app = MultiPage(app_name = "Mushroom Safety")
 
 app.add_page("Project Summary", page_summary_body)
